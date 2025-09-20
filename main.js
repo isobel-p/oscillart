f3186c9e145edc431d93cf364f4953fd5279cf7a
Isobel Pang <173300585+isobel-p@users.noreply.github.com>
2025-09-20 20:33:36 +0100

    Add recording feature

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ var interval = null;
 const input = document.getElementById('input');
 const colour_picker = document.getElementById("colour");
 const volume = document.getElementById('vol-slider')
+const recording_toggle = document.getElementById('record');
+
 var reset = false;
 var x = 0;
 var y = 0;
 var freq = 0;
 var timepernote = 0;
 var length = 0;
+var blob, recorder = null;
+var chunks = [];
 
 // create web audio api elements
 const audioCtx = new AudioContext();
@@ -103,3 +107,41 @@ function handle() {
         }
     }, 2000)
 } 
+
+function startRecording(){
+    const canvasStream = canvas.captureStream(20);
+    const audioDestination = audioCtx.createMediaStreamDestination();
+    const combinedStream = new MediaStream();
+    canvasStream.getVideoTracks().forEach(track => combinedStream.addTrack(track));
+    audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
+    gainNode.connect(audioDestination);
+    recorder = new MediaRecorder(combinedStream, {mimeType:'video/webm'});
+    recorder.ondataavailable = e => {
+        if (e.data.size > 0) {
+        chunks.push(e.data);
+        }
+        };
+
+
+        recorder.onstop = () => {
+        const blob = new Blob(chunks, { type: 'video/webm' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recording.webm';
+        a.click();
+        URL.revokeObjectURL(url);
+    };
+    recorder.start()
+}
+
+function toggle(){
+    is_recording != is_recording;
+    if(is_recording){
+        recording_toggle.innerHTML = "Stop Recording";
+        startRecording();
+    } else {
+        recording_toggle.innerHTML = "Start Recording";
+        recorder.stop();
+    }
+}
